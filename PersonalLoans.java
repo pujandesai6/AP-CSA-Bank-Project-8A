1a86376a43cebdc6c8672a14c7592e2080812c35
sebbbbbbbbb <121190673+sebbbbbbbbb@users.noreply.github.com>
2023-02-07 11:01:19 -0800

    Update PersonalLoans.java

--- PersonalLoans.java
+++ PersonalLoans.java
@@ -1,21 +1,24 @@
 import java.util.*;
+import java.lang.Math;
 
 class Loan
 {
-	private static ArrayList<Loan> loanList = new ArrayList<Loan>();
+    private static ArrayList<Loan> loanList = new ArrayList<Loan>();
     private String fullLegalName;
     private String loanName;
     private double principle;
     private double monthlyPay;
+    private double totalPay;
     private double interestRate;
     private int loanLength;
     private static int creditScore;
 
-    public Loan(String loanName, double principle, double monthlyPay, int loanLength, double interestRate)
+    public Loan(String loanName, double principle, double totalPay, double monthlyPay, int loanLength, double interestRate)
     {
         this.fullLegalName = "GetFullName"; //Get from Customer
         this.loanName = loanName;
         this.principle = principle;
+	this.totalPay = totalPay;
         this.monthlyPay = monthlyPay;
         this.loanLength = loanLength;
         this.creditScore = 700; //Get from Customer
@@ -53,9 +56,10 @@ class Loan
         System.out.print("How many years do you want your loan term to be?: ");
         loanTerm = adhi.nextInt();
    
-        double interest = ((1000.0 - (double)creditScore) * 0.001) * (1.0 + (double)loanTerm / 20); //Not Working
-        double monthlyPayments = (interest * loanPrinciple)/ 12;
-        loanList.add(new Loan(name,loanPrinciple,monthlyPayments,loanTerm,interest));
+        double interest = 1.0 + ((1000.0 - (double)creditScore) * 0.0001) * (1.0 + (double)loanTerm * 0.1); //Not Working
+	double tPay = Math.pow(interest, (double) loanTerm) * loanPrinciple
+        double monthlyPayments = tPay / (loanTerm * 12);
+        loanList.add(new Loan(name,loanPrinciple,tPay,monthlyPayments,loanTerm,interest));
         System.out.println("New loan created!");
     }
     public static void getUserLoans()
@@ -69,26 +73,55 @@ class Loan
         }
         	System.out.println("================================");
     }
-    public void payLoan()
+    public void totalMinusMonthly()
+    {
+	   totalPay -= monthlyPay;
+	   monthlyPay = 0;
+    }
+    public static void loanTotalPay(int choice)
+    {
+	    Loan loanChoice = loanList.get(choice - 1);
+	    loanChoice.totalMinusMonthly();
+	    System.out.print("Remaining Balance: $" + loanChoice.getBalance());
+    }
+    public double getMonthlyPay()
+    {
+	    return monthlyPay;
+    }
+    public static double getChoiceMonthlyPay(int x)
+    {
+	    Loan choice = loanList.get(x - 1);
+	    return choice.getMonthlyPay();
+    }
+    public static void payLoan()
     {
     	Scanner ashleySmells = new Scanner(System.in);
     	int loanChoice, acctChoice;
+	
     	System.out.println("Choose a Loan # from the list to make a payment on: ");
     	Loan.getUserLoans();
     	loanChoice = ashleySmells.nextInt();
     	System.out.println("Choose an Account # from the list to pay from: ");
     	SavingsAccount.getUserSavings();
     	acctChoice = ashleySmells.nextInt();
-    	
-    	
+	if(SavingsAccount.getSavingsBal(acctChoice) >= Loan.getChoiceMonthlyPay(loanChoice)
+		Loan.loanTotalPay(loanChoice);
+		SavingsAccount.withdraw(acctChoice, Loan.getChoiceMonthlyPay());
+	else
+	   
+	
+    }
+    public double getBalance()
+    {
+	    return totalPay();
     }
     public void displayLoanDetails() 
     {
-    	double displayInterest = (interestRate - 1) * 100;
+    	double displayInterest = (interestRate - 1.0) * 100.0;
         System.out.println("Holder: " + fullLegalName);
         System.out.println("Purpose: " + loanName);
         System.out.println("Original Term: " + loanLength);
-        System.out.println("Remaining balance: $" + principle);
+        System.out.println("Remaining balance: $" + totalPay);
         System.out.println("Remaining balance this month: $" + monthlyPay);
         System.out.println("Interest: " + displayInterest + "%");
         System.out.println(interestRate);
